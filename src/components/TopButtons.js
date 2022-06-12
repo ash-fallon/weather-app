615bd666d3fe7be1cf07fc51621e8e8ba193ee92
Ash Fallon <ashfallon23@gmail.com>
2022-06-12 12:51:58 +0100

    Added TopButtons component with basic styling

---
+++ src/components/TopButtons.js
@@ -0,0 +1,13 @@
+const TopButtons = () => {
+  const cities = [
+    { id: 1, title: 'London' },
+    { id: 2, title: 'Sydney' },
+    { id: 3, title: 'Tokyo' },
+    { id: 4, title: 'Toronto' },
+    { id: 5, title: 'Paris' },
+  ];
+
+  return <div className='flex items-center justify-center my-6'></div>;
+};
+
+export default TopButtons;
