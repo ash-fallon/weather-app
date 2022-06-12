606dcccde6dd454e439f2a9cf7934569d4409a33
Ash Fallon <ashfallon23@gmail.com>
2022-06-12 12:31:04 +0100

    Added initial styling to container div

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import UilReact from '@iconscout/react-unicons/icons/uil-react';
+
 const App = () => {
-  return <h1 className='text-4xl text-center'>Hello World</h1>;
+  return (
+    <>
+      <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400'></div>
+    </>
+  );
 };
 
 export default App;
