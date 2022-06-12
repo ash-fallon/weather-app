0fb6fabb155537a5424d81c41838dab72f033692
Ash Fallon <ashfallon23@gmail.com>
2022-06-12 20:01:30 +0100

    Added dynamic data to Forecast

--- src/App.js
+++ src/App.js
@@ -32,8 +32,8 @@ const App = () => {
           <>
             <TimeAndLocation weather={weather} />
             <TemperatureAndDetails weather={weather} />
-            <Forecast title='Hourly Forecast' />
-            <Forecast title='Daily Forecast' />
+            <Forecast title='Hourly Forecast' items={weather.hourly} />
+            <Forecast title='Daily Forecast' items={weather.daily} />
           </>
         )}
       </div>
--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,4 +1,6 @@
-const Forecast = ({ title }) => {
+import { iconUrlFromCode } from '../services/weatherService';
+
+const Forecast = ({ title, items }) => {
   return (
     <div>
       <div className='flex items-center justify-start mt-6'>
@@ -9,16 +11,17 @@ const Forecast = ({ title }) => {
 
       <div className='flex flex-row items-center justify-between text-white'>
         {/* Forecast list */}
-        {/* Hard coded list for prototyping purposes, will add mapping later */}
-        <div className='flex flex-col items-center justify-center'>
-          <p className='font-light text-sm'>04:30 PM</p>
-          <img
-            src='http://openweathermap.org/img/wn/01d@2x.png'
-            alt=''
-            className='w-12 my-1'
-          />
-          <p className='font-medium'>22°</p>
-        </div>
+        {items.map(item => (
+          <div className='flex flex-col items-center justify-center'>
+            <p className='font-light text-sm'>{item.title}</p>
+            <img
+              src={iconUrlFromCode(item.icon)}
+              alt=''
+              className='w-12 my-1'
+            />
+            <p className='font-medium'>{`${item.temp.toFixed()}°`}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
