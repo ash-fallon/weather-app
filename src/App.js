6c22b9c19acf1d8e9653420575d80437bcfafdbd
Ash Fallon <ashfallon23@gmail.com>
2022-06-12 14:04:53 +0100

    Added TimeAndLocation component with initial dummy data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import UilReact from '@iconscout/react-unicons/icons/uil-react';
 import TopButtons from './components/TopButtons';
 import Inputs from './components/Inputs';
+import TimeAndLocation from './components/TimeAndLocation';
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 rounded bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400'>
         <TopButtons />
         <Inputs />
+        <TimeAndLocation />
       </div>
     </>
   );
