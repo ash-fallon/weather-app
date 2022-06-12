443cd13e5a06ecb4f6241028a6c35a38e8a38f40
Ash Fallon <ashfallon23@gmail.com>
2022-06-12 18:05:09 +0100

    Added formatting logic to weatherService

--- src/App.js
+++ src/App.js
@@ -4,8 +4,16 @@ import Inputs from './components/Inputs';
 import TimeAndLocation from './components/TimeAndLocation';
 import TemperatureAndDetails from './components/TemperatureAndDetails';
 import Forecast from './components/Forecast';
+import getFormattedWeatherData from './services/weatherService';
 
 const App = () => {
+  const fetchWeather = async () => {
+    const data = await getFormattedWeatherData({ q: 'berlin' });
+    console.log(data);
+  };
+
+  fetchWeather();
+
   return (
     <>
       <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 rounded bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400'>
--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -10,4 +10,44 @@ const getWeatherData = (infoType, searchParams) => {
     .then(data => data);
 };
 
-export default getWeatherData;
+const formatCurrentWeather = data => {
+  const {
+    coord: { lat, lon },
+    main: { temp, feels_like, temp_min, temp_max, humidity },
+    name,
+    dt,
+    sys: { country, sunrise, sunset },
+    weather,
+    wind: { speed },
+  } = data;
+
+  const { main: details, icon } = weather[0];
+
+  return {
+    lat,
+    lon,
+    temp,
+    feels_like,
+    temp_min,
+    temp_max,
+    humidity,
+    name,
+    dt,
+    country,
+    sunrise,
+    sunset,
+    details,
+    icon,
+    speed,
+  };
+};
+
+const getFormattedWeatherData = searchParams => {
+  const formattedCurrentWeather = getWeatherData('weather', searchParams).then(
+    formatCurrentWeather,
+  );
+
+  return formattedCurrentWeather;
+};
+
+export default getFormattedWeatherData;
