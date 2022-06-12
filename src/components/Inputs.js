a1dd7e54d11135f0ec63d50f5e942ac09567580f
Ash Fallon <ashfallon23@gmail.com>
2022-06-12 20:24:35 +0100

    Added click logic to Inputs

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -1,11 +1,41 @@
+import { useState } from 'react';
 import { UilSearch, UilLocationPoint } from '@iconscout/react-unicons';
 
-const Inputs = () => {
+const Inputs = ({ setQuery, units, setUnits }) => {
+  const [city, setCity] = useState('');
+
+  const searchClickHandler = () => {
+    if (city !== '') {
+      setQuery({ q: city });
+    }
+  };
+
+  const locationClickHandler = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        let lat = position.coords.latitude;
+        let lon = position.coords.longitude;
+
+        setQuery({ lat, lon });
+      });
+    }
+  };
+
+  const unitsClickHandler = e => {
+    const selectedUnit = e.currentTarget.name;
+
+    if (units !== selectedUnit) {
+      setUnits(selectedUnit);
+    }
+  };
+
   return (
     <div className='flex flex-row justify-center my-6'>
       {/* Search bar input */}
       <div className='flex flex-row w-3/4 items-center justify-center space-x-4'>
         <input
+          value={city}
+          onChange={e => setCity(e.target.value)}
           type='text'
           placeholder='Search for cities...'
           className='text-xl font-white p-2 w-full shadow-xl capitalize rounded-sm focus:outline-none placeholder:lowercase'
@@ -14,10 +44,12 @@ const Inputs = () => {
         <UilSearch
           size={25}
           className='text-white cursor-pointer transition ease-out hover:scale-125'
+          onClick={searchClickHandler}
         />
         <UilLocationPoint
           size={25}
           className='text-white cursor-pointer transition ease-out hover:scale-125'
+          onClick={locationClickHandler}
         />
       </div>
       <div className='flex flex-row w-1/4 items-center justify-center'>
@@ -25,6 +57,7 @@ const Inputs = () => {
         <button
           name='metric'
           className='text-xl text-white font-medium transition ease-out hover:scale-125'
+          onClick={unitsClickHandler}
         >
           °C
         </button>
@@ -32,6 +65,7 @@ const Inputs = () => {
         <button
           name='imperial'
           className='text-xl text-white font-medium transition ease-out hover:scale-125'
+          onClick={unitsClickHandler}
         >
           °F
         </button>
