cca4285af3dfa883b029beb9d50e4d4dc072a4e6
Ash Fallon <ashfallon23@gmail.com>
2022-06-12 14:14:28 +0100

    Added Poppins font, added TemperatureAndDetails component

---
+++ src/components/TemperatureAndDetails.js
@@ -0,0 +1,5 @@
+const TemperatureAndDetails = () => {
+  return <div>TemperatureAndDetails</div>;
+};
+
+export default TemperatureAndDetails;
--- src/components/TimeAndLocation.js
+++ src/components/TimeAndLocation.js
@@ -2,12 +2,13 @@ const TimeAndLocation = () => {
   return (
     <div>
       <div className='flex items-center justify-center my-6'>
+        {/* Date and time */}
         <p className='text-white text-xl font-extralight'>
           Tuesday, 31 May 2022 | Local time: 12:46 PM
         </p>
       </div>
-
       <div className='flex items-center justify-center my-3'>
+        {/* Current selected location */}
         <p className='text-white text-3xl font-medium'>Berlin, DE</p>
       </div>
     </div>
